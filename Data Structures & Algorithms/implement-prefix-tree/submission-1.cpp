bde43aa0387f114013b858869b037b0a13061a1b
AsterBluem <81480518+AnshuAyush@users.noreply.github.com>
2026-07-01 17:44:03 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+class PrefixTree {
+public:
+    map <string, int> mp;
+    PrefixTree() {
+
+        
+    }
+    
+    void insert(string word) {
+        mp[word] += 1;
+    }
+    
+    bool search(string word) {
+        return mp.count(word);
+    }
+    
+   bool startsWith(string prefix) {
+
+    for (auto &it : mp) {
+
+        string word = it.first;
+
+        if (word.size() >= prefix.size() &&word.substr(0, prefix.size()) == prefix) {
+            return true;
+        }
+    }
+
+    return false;
+}
+};
