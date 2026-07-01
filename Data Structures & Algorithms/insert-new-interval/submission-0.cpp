14487ea1ca11737785beeeea71d09e212e2d0fa1
AsterBluem <81480518+AnshuAyush@users.noreply.github.com>
2026-07-01 11:39:21 +0530

    Add: insert-new-interval - submission-0

---
+++ Data Structures & Algorithms/insert-new-interval/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        
+        intervals.push_back(newInterval);
+
+        sort(intervals.begin(), intervals.end());
+
+        int j = 0;
+        vector <vector<int>> ans;
+        ans.push_back(intervals[0]);
+
+        for(int i = 1; i < intervals.size(); i++){
+            if(ans[j][1] >= intervals[i][0]){
+                ans[j][1] = max(ans[j][1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+                j += 1;
+            }
+        }
+        return ans;
+    }
+};
