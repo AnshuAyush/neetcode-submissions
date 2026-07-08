3c27f8be397037e5ca5e67ae751b2a83e80123c0
AsterBluem <81480518+AnshuAyush@users.noreply.github.com>
2026-07-08 16:26:24 +0530

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+
+    vector <vector<int>> ans;
+
+    void solve(vector<int> &nums, int l){
+        
+        if(l == nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i = l; i < nums.size(); i++){
+            swap(nums[i], nums[l]);
+            solve(nums, l + 1);
+            swap(nums[i], nums[l]);
+        }
+      
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        solve(nums, 0);
+        return ans;
+        
+    }
+};
