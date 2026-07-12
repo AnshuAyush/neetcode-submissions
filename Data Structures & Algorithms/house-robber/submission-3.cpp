21498670c0903ebc4f19aa5d03d82f28738d383e
AsterBluem <81480518+AnshuAyush@users.noreply.github.com>
2026-07-12 20:07:54 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        vector <int> dp(200, -1);
+        return dfs(nums, 0, dp);
+    }
+
+    int dfs(vector<int>& nums, int i, vector<int> &dp) {
+        if (i >= nums.size()) {
+            return 0;
+        }
+        if(dp[i] != -1)return dp[i];
+
+        dp[i] = max(dfs(nums, i + 1, dp), nums[i] + dfs(nums, i + 2, dp));
+        return dp[i];
+    }
+};
