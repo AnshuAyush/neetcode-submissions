37f89fb5029a3261083f7a302c356bcb1ef121a6
AsterBluem <81480518+AnshuAyush@users.noreply.github.com>
2026-08-08 04:48:59 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+        int maxProfit(vector<int>& prices) {
+        // prices       10,1,5,6,7,1
+        // max_suffix = 10 7 7 7 7 1
+        
+        vector <int> max_suffix(prices.size(), 0);
+        int maxi = INT_MIN;
+
+        for(int i = prices.size() - 1; i >= 0; i--){
+            maxi = max(prices[i], maxi);
+            max_suffix[i] = maxi;
+        }
+        int ans = 0;
+ 
+        for(int i = 0; i < prices.size() - 1; i++){
+            
+                ans = max(ans, max_suffix[i] - prices[i] );
+            
+        }
+        return ans;
+    }
+};
